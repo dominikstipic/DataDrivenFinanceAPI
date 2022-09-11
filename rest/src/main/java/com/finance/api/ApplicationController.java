d14b44a3795feef988c9b9bb18df7d8980a4c750
dominikstipic <dominikstipic@gmail.com>
2022-09-11 12:22:59 +0200

    interfaces for processing and service

--- rest/src/main/java/com/finance/api/ApplicationController.java
+++ rest/src/main/java/com/finance/api/ApplicationController.java
@@ -9,7 +9,6 @@ public class ApplicationController extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         return Set.of(
-                HypothesisTestingController.class,
                 DataRetrieverController.class,
                 StatisticsController.class
         );
